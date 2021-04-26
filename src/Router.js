1a0b95c0192bac5516667c57ff723db490ae059a
FaziletKosure <faziletkosure1@gmail.com>
2021-04-26 22:44:07 +0200

    router updated

--- src/Router.js
+++ src/Router.js
@@ -3,15 +3,12 @@ import {View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
-const Stack = createStackNavigator();
 import Home from './components/Home';
 import About from './components/About';
 import Tutors from './components/Tutors';
 import TermsAndConditions from './components/TermsAndConditions';
-// import HangMan from './pages/HangMan';
-// import CafePage from './pages/CafePage';
-// import CafeList from './pages/CafeList';
-// import App from '../App';
+
+const Stack = createStackNavigator();
 
 function Router() {
   return (
@@ -22,9 +19,6 @@ function Router() {
           gestureEnabled: true,
           headerShown: false,
         }}>
-        {/* <Stack.Screen name="ImageGalleryPage" component={ImageGallery} />
-        <Stack.Screen name="WeatherPage" component={App} />
-        <Stack.Screen name="WebPage" component={WebPape} /> */}
         <Stack.Screen name="TutorsPage" component={Tutors} />
         <Stack.Screen name="AboutPage" component={About} />
 
